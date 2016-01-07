e416bf15bd4e93314c05c078f3f052cafb6ebde9
abouzol <abouzol@gmail.com>
2016-01-07 16:29:24 -0600

    (bug) fixed port

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
-
+var port = process.env.PORT || 8000;
 //mongoose.connect('mongodb://localhost/tacocat');
 
 require('./config/middleware.js')(app, express);
 
-app.listen(8000);
+app.listen(port);
 
 module.exports = app;
