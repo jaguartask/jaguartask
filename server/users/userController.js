2458bf1dcf5edac3de1d5320fda2a6c85056b853
abouzol <abouzol@gmail.com>
2016-01-07 21:53:33 -0600

    (feat) signup test should add newUser to DB.

--- server/users/userController.js
+++ server/users/userController.js
@@ -10,5 +10,22 @@ module.exports = {
     var password = req.body.password;
 
     var findUser = promise.bind(User.findOne, User);
-  }
+  },
+
+  signup: function(req, res, next){
+    var username = req.body.username;
+    var password = req.body.password;
+    var create;
+    var newUser;
+
+    var findUser = promise.bind(User.findUser, User);
+
+    create = promise.bind(User.create, User);
+    newUser = {
+      username: username,
+      password: password
+    };
+    return create(newUser);
+  },
+
 };
