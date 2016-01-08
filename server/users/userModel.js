4e723c1a284654547394a2977054dd5a4404d0ec
abouzol <abouzol@gmail.com>
2016-01-08 14:10:13 -0600

    (feat) added bcrypt and comparing in the works

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
 
 var UserSchema = new mongoose.Schema({
   username: {
@@ -9,10 +12,28 @@ var UserSchema = new mongoose.Schema({
 
   password: {
     type: String,
-    //For test purposes will change back to ***true***
-    required: false
+    required: true
   },
 });
 
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+    if(err) return next(err);
+
+    user.password = hash;
+    next();
+  });
+});
+
+  UserSchema.methods.comparePassword = function(canadidtatePassword, cb){
+    bcrypt.compare(canadidtatePassword, this.password, function(err, isMatch){
+      if(err) return cb(err);
+      cb(null, isMatch);
+    });
+  };
 
 module.exports = mongoose.model('users', UserSchema);
