b212d78ed72650f11ce421600062afde73691619
GMeyr <gabrielmeyr@gmail.com>
2016-01-08 15:22:43 -0500

    linked to app w/o login, updated style

--- client/home/home.js
+++ client/home/home.js
@@ -1,5 +1,8 @@
 angular.module('jaguarTask.home', [])
 
-.controller('HomePageController', function($scope){
-  
+.controller('HomePageController', function($scope, $location){
+  $scope.routeToMain = function(){
+    console.log("routeToMain click");
+    //$location.path('main');
+    };
 });
