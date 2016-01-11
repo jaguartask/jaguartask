f879dfb6e8d9bd3323d84b0ee343948ca8caac38
abouzol <abouzol@gmail.com>
2016-01-10 22:10:29 -0600

    Middleware for task

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,6 +5,7 @@ var helpers = require('./utils.js');
 
 module.exports = function(app, express){
   var userRouter = express.Router();
+  var taskRouter = express.Router();
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
@@ -13,12 +14,13 @@ module.exports = function(app, express){
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
+  app.use('/api/task/', taskRouter);
   app.use('/api/user/', userRouter);
   // app.use('/singin', userRouter);
   // app.post('/', function(req, res) {
   //   res.json({loggedin: true})
   // })
-
+  require('../task/taskRoutes.js')(taskRouter);
   require('../users/userRoutes.js')(userRouter);
 
 };
