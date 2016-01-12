6812019647c659d73ae7bb7037197bb7e7cf95c3
darwin <oleg.umarov@gmail.com>
2016-01-11 20:20:51 -0600

    (feat) Data is available

--- server/taskList/taskListController.js
+++ server/taskList/taskListController.js
@@ -1,13 +1,16 @@
 var List = require('./taskListModel.js');
+var Task = require('../task/taskModel.js');
 
 module.exports = {
 
   save: function(req, res, next){
-    
   },
 
   get: function(req, res, next){
-
+    Task.find({}).exec()
+      .then(function(user) {
+        res.json(user)
+      });
   },
 
 
