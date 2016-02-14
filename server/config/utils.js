83d282cc0c657423dc2fb07ceee7ae7e9ed09611
Gabriel Meyr <gabrielmeyr@gmail.com>
2016-02-13 22:32:48 -0500

    (refactor) Fix spacing

--- server/config/utils.js
+++ server/config/utils.js
@@ -1,14 +1,9 @@
 module.exports = {
   errorLogger: function (error, req, res, next) {
-    // log the error then send it to the next middleware in
-    // middleware.js
-
     console.error(error.stack);
     next(error);
   },
   errorHandler: function (error, req, res, next) {
-    // send error message to client
-    // message for gracefull error handling on app
     res.send(500, {error: error.message});
   },
 
@@ -17,7 +12,6 @@ module.exports = {
     var user;
 
     try {
-      // for use inside our controllers
       req.user = user;
       next();
     } catch(error) {
