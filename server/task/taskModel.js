e27c528ccd6e151d1d36649d4f72e7fd5094e4e5
Gabriel Meyr <gabrielmeyr@gmail.com>
2016-02-13 22:37:19 -0500

    (refactor) Fix spacing

--- server/task/taskModel.js
+++ server/task/taskModel.js
@@ -6,33 +6,26 @@ var taskSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-
   notes: {
     type: String,
     required: false
   },
-
   done:{
     type: Boolean,
     required:true
   },
-
-
   dueDate:{
     type: Date,
     required: false
   },
-
   priority:{
     type: String,
     required: false
   },
-
   user:{
     type: Array,
     required: true
   },
-
   lists:{
     type: Array,
     required: false
