4d1dce49a349d474451eda8816af8dd7b5799643
Gabriel Meyr <gabrielmeyr@gmail.com>
2016-02-13 22:42:26 -0500

    (refactor) Remove unnecessary comment

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -2,12 +2,9 @@ var userController = require('./userController.js');
 
 module.exports = function(app) {
 
-  // console.log(app);
   app.post('/signup', userController.signup);
   app.post('/signin', userController.signin);
-  // console.log(userController.signup);
   // app.post('/', function(req, res, next) {
   //   res.json({connected: true});
   // });
-
 };
